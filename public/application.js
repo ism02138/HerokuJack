df66d3d87f7c58b44702fd41d0316acddda95112
Ian Moulton <ian.moulton@gmail.com>
2013-07-04 15:34:34 -0400

    fixed js

--- public/application.js
+++ public/application.js
@@ -1,39 +1,35 @@
 $(document).ready(function() {
-	$("form#hit_form input").click(function() {
-		$(document).on("click", "form#hit_form input", function() {
-			alert("player hits!");
-			$.ajax({
-				type: 'POST', 
-				url: '/player_hit'
-			}).done(function(msg) {
-				$("#game").replaceWith(msg);
-			});
-			return false;
+	$(document).on("click", "form#hit_form input", function() {
+		
+		$.ajax({
+			type: 'POST', 
+			url: '/player_hit'
+		}).done(function(msg) {
+		alert("player hits!");
+			$("#game").replaceWith(msg);
 		});
+		return false;
 	});
 
-	$("form#stay_form input").click(function() {
-		$(document).on("click", "form#stay_form input", function () {
-			alert("player stays!");
-			$.ajax({
-				type: 'POST',
-				url: '/player_stay'
-			}).done(function(msg) {
-				$("#game").replaceWith(msg);
-			});
-			return false;
+	$(document).on("click", "form#stay_form input", function () {
+		alert("player stays!");
+		$.ajax({
+			type: 'POST',
+			url: '/player_stay'
+		}).done(function(msg) {
+			$("#game").replaceWith(msg);
 		});
+		return false;
 	});
-	$("form#computer_hit_form input").click(function() {
-		$(document).on("click", "form#computer_hit_form input", function () {
-			alert("computer hits!");
-			$.ajax({
-				type: 'POST',
-				url: '/computer_hit'
-			}).done(function(msg) {
-				$("#game").replaceWith(msg);
-			});
-			return false;
+
+	$(document).on("click", "form#computer_hit_form input", function () {
+		alert("computer hits!");
+		$.ajax({
+			type: 'POST',
+			url: '/computer_hit'
+		}).done(function(msg) {
+			$("#game").replaceWith(msg);
 		});
+		return false;
 	});
 });
