48fd5e36a73d708daf85ef7375033eda675b62ce
Ian Moulton <ian.moulton@gmail.com>
2013-07-04 14:52:58 -0400

    trouble

--- public/application.js
+++ public/application.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
 	$("form#hit_form input").click(function() {
 		$(document).on("click", "form#hit_form input", function() {
+			alert("player hits!");
 			$.ajax({
 				type: 'POST', 
 				url: '/player_hit'
 			}).done(function(msg) {
-				$("game").replaceWith(msg);
+				$("#game").replaceWith(msg);
 			});
 			return false;
 		});
 	});
 
 	$("form#stay_form input").click(function() {
-		$(document).on("click", "form#stay_form", function () {
+		$(document).on("click", "form#stay_form input", function () {
+			alert("player stays!");
 			$.ajax({
 				type: 'POST',
 				url: '/player_stay'
@@ -23,7 +25,8 @@ $(document).ready(function() {
 		});
 	});
 	$("form#computer_hit_form input").click(function() {
-		$(document).on("click", "form#computer_hit_form", function () {
+		$(document).on("click", "form#computer_hit_form input", function () {
+			alert("computer hits!");
 			$.ajax({
 				type: 'POST',
 				url: '/computer_hit'
