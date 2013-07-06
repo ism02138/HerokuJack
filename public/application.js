ec68a804f62adfc880dbf4ceebc4e8e1be294056
Ian Moulton <ian.moulton@gmail.com>
2013-07-06 12:38:30 -0400

    final

--- public/application.js
+++ public/application.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
 	$(document).on("click", "form#hit_form input", function() {
-		
 		$.ajax({
 			type: 'POST', 
 			url: '/player_hit'
 		}).done(function(msg) {
-		alert("player hits!");
 			$("#game").replaceWith(msg);
 		});
 		return false;
 	});
 
 	$(document).on("click", "form#stay_form input", function () {
-		alert("player stays!");
 		$.ajax({
 			type: 'POST',
 			url: '/player_stay'
@@ -23,7 +20,6 @@ $(document).ready(function() {
 	});
 
 	$(document).on("click", "form#computer_hit_form input", function () {
-		alert("computer hits!");
 		$.ajax({
 			type: 'POST',
 			url: '/computer_hit'
